eb85e548cba73c2ef7ba594a2464a3c94bac6228
Anna Johannesson <an8463jo-s@student.lu.se>
2024-04-01 15:02:26 +0200

    recive and send

--- Client/client.py
+++ Client/client.py
@@ -1,21 +1,46 @@
 import sys
 import struct
 import time
+from multiprocessing import Process
 sys.path.append('../')
 
 import common
 
 radio_send, radio_recv = common.setup_radios()
 
-radio_send.setChannel(77)
-radio_recv.setChannel(76)
+radio_send.setChannel(76)
+radio_recv.setChannel(77)
 
 address = [b"1Node"]
+
+def send():
+    radio_send.open_tx_pipe(address[0])
+    radio_send.listen = False
+
+    radio_send.payload_size = struct.calcsize('<f')
+    val = 0.0
+
+    count = 10
+    while count:
+        buf = struct.pack('<f', val)
+        start_timer = time.monotonic_ns()
+        result = radio_send.write(buf)
+        end_timer = time.monotonic_ns()
+        if not result:
+            print("Transmission failed or timed out")
+        else:
+            print(
+                "Transmission successful! Time to Transmit:",
+                f"{(end_timer - start_timer) / 1000} us. Sent: {val}",
+            )
+            val += 0.11
+        time.sleep(1)
+        count -= 1
 
 
 def recieve():
     timeout = 10
-    radio_recv.open_rx_pipe(0, address[0])
+    radio_recv.open_rx_pipe(0, b"2Node")
     radio_recv.payload_size = struct.calcsize("<f")
     payload = [0.0]
     
@@ -35,6 +60,14 @@ def recieve():
             print(f"Received {length} bytes on pipe {pipe_number}: {payload[0]}")
             start = time.monotonic()  # reset the timeout timer
 
-recieve()
+if __name__ == '__main__':
+    p1 = Process(target=send, args=())
+    p2 = Process(target=recieve, args=())
+    p1.start()
+    p2.start()
+    
+    p1.join()
+    p2.join()
+
    
 
